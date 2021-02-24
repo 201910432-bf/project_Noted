6225afb6db2bd0e207afccd0a560e42f0322f297
ber <bherfelismenia@gmail.com>
2021-02-24 16:09:17 +0800

    creating edit function

--- controller/controller.js
+++ controller/controller.js
@@ -47,7 +47,7 @@ const getNoteData = (key, data) => {
                 </div>
                 <div class="main__content__remaining__edit">
                     <span>3 tasks remaining</span>
-                    <a href="">Edit</a>
+                    <a href="javascript:void(0)" onclick="editClick()">Edit</a>
                 </div>
                 <div class="main__content__add">
                     <input type="text" id="addListText" value="">
@@ -89,17 +89,23 @@ const getNoteData = (key, data) => {
                 )
               ) {
                 template += `
+                <div class="checkBoxLabel">
                   <label for="">
                   <input type="checkbox" value="${splitDataKey[i]},${splitData[i]}" id="${splitDataKey[i]}checkBox" onclick="checkboxClick('${splitDataKey[i]}checkBox','')" checked>
                   <span id="${splitDataKey[i]}checkBoxSpan" class="checkedList" >${splitData[i]}</span>
                   </label>
+                  <a class="removeBtn get${splitDataKey[i]}" href="javascript:void(0)" onclick="removeClick('${splitDataKey[i]}')" id="checkBoxRemove" value="${splitDataKey[i]}"><img width="15" src="../assets/images/removeIcon.png" alt=""></a>
+                </div>
                   `;
               } else {
                 template += `
-                <label for="">
-                <input type="checkbox" value="${splitDataKey[i]},${splitData[i]}" id="${splitDataKey[i]}checkBox" onclick="checkboxClick('${splitDataKey[i]}checkBox','')" >
-                <span id="${splitDataKey[i]}checkBoxSpan">${splitData[i]}</span>
-                </label>
+                <div class="checkBoxLabel">
+                  <label for="">
+                  <input type="checkbox" value="${splitDataKey[i]},${splitData[i]}" id="${splitDataKey[i]}checkBox" onclick="checkboxClick('${splitDataKey[i]}checkBox','')" >
+                  <span id="${splitDataKey[i]}checkBoxSpan">${splitData[i]}</span>
+                  </label>
+                  <a class="removeBtn get${splitDataKey[i]}" href="javascript:void(0)" onclick="removeClick('${splitDataKey[i]}')" id="checkBoxRemove" value="${splitDataKey[i]}"><img width="15" src="../assets/images/removeIcon.png" alt=""></a>
+                </div>
                 `;
               }
             }
@@ -133,12 +139,16 @@ const addList = (listData) => {
     }
 
     var stringTemp = `
-    <label for="">
-        <input type="checkbox" value="${
-          newId + "," + list
-        }" id="${newId}checkBox" onclick="checkboxClick('${newId}checkBox','')" >
-        <span id='${newId}checkBoxSpan'>${list}</span>
-    </label>`;
+    <div class="checkBoxLabel">
+      <label for="">
+          <input type="checkbox" value="${
+            newId + "," + list
+          }" id="${newId}checkBox" onclick="checkboxClick('${newId}checkBox','')" >
+          <span id='${newId}checkBoxSpan'>${list}</span>
+      </label>
+      <a href="#"><img width="15" src="../assets/images/removeIcon.png" alt=""></a>
+    </div>
+    `;
 
     divList.innerHTML += stringTemp;
     document.getElementById("addListText").value = "";
@@ -147,6 +157,7 @@ const addList = (listData) => {
 
 const checkboxClick = (idKey, originalData) => {
   console.log(originalData);
+
   if (globalDataVariable.length == 0 && originalData != "") {
     globalDataVariable = JSON.parse(originalData);
   }
@@ -190,20 +201,27 @@ const passtoUrl = (originalData, key, title) => {
     console.log(globalDataVariable);
   }
 
-  const fetchList = document
-    .getElementById("wrapLabel")
-    .getElementsByTagName("input");
+  // const fetchList = document
+  //   .getElementById("wrapLabel")
+  //   .getElementsByTagName("input");
+
+  const fetchList = document.querySelectorAll('input[type="checkbox"]');
+  const allList = document.querySelectorAll(".spanListText");
 
   var idArray = [];
   var valueArray = [];
   for (let i = 0; i < fetchList.length; i++) {
     const splitData = fetchList[i].value.split(",");
     idArray.push(splitData[0]);
-    valueArray.push(splitData[1]);
+    valueArray.push(allList[i].innerHTML);
   }
 
-  console.log(idArray);
-  var request = new XMLHttpRequest();
+  console.log(valueArray); //emd hr
+  //if click edit record the last change of all textfield
+  //if click save while textfield is active get all the data of text field and make it as valueArray
+  //update all change to all string js template
+
+  // var request = new XMLHttpRequest();
 
   // request.onreadystatechange = function () {
   //   if (request.readyState === XMLHttpRequest.DONE) {
@@ -211,15 +229,15 @@ const passtoUrl = (originalData, key, title) => {
   //     }
   //   }
   // };
-  request.open(
-    "POST",
-    `http://localhost:5000/savenote/note/?objectChecked=${JSON.stringify(
-      globalDataVariable
-    )}&arrayId=${idArray}&arrayValue=${valueArray}&title=${title}&key=${key}`,
-    true
-  );
+  // request.open(
+  //   "POST",
+  //   `http://localhost:5000/savenote/note/?objectChecked=${JSON.stringify(
+  //     globalDataVariable
+  //   )}&arrayId=${idArray}&arrayValue=${valueArray}&title=${title}&key=${key}`,
+  //   true
+  // );
 
-  request.send(null);
+  // request.send(null);
 };
 
 const addNote = () => {
@@ -239,4 +257,41 @@ const addNote = () => {
     true
   );
   request.send(null);
+};
+
+const editClick = () => {
+  const checkBoxRemove = document.querySelectorAll(".removeBtn");
+  const spanListText = document.querySelectorAll(".spanListText");
+  const editListTextInput = document.getElementsByClassName(
+    "editListTextInput"
+  );
+
+  console.log(editListTextInput);
+  if (
+    checkBoxRemove[0].style.visibility == "" ||
+    checkBoxRemove[0].style.visibility == "hidden"
+  ) {
+    for (let data of checkBoxRemove) {
+      data.style.visibility = "visible";
+    }
+    for (let data of editListTextInput) {
+      data.style.visibility = "visible";
+    }
+    for (let data of spanListText) {
+      data.style.display = "none";
+    }
+  } else {
+    for (let data of checkBoxRemove) {
+      data.style.visibility = "hidden";
+    }
+    for (let data of editListTextInput) {
+      data.style.visibility = "hidden";
+    }
+    for (let data of spanListText) {
+      data.style.display = "inline-block";
+    }
+  }
+};
+const removeClick = (data) => {
+  console.log(data);
 };
